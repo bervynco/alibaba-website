5a0f0c075f3af4a0785996eb7aacc675de7cc772
Bervyn Co <bervyn_co2010@yahoo.com>
2018-01-06 14:52:32 +0800

    WS for site data

--- application/models/Site_model.php
+++ application/models/Site_model.php
@@ -1,22 +1,63 @@
 <?php
 class site_model extends CI_Model {
-    function selectAllSiteData(){
-        $homeData = $this->selectHomeData();
-        print_r($homeData);
-        // return($query->num_rows() > 0) ? $query->result_array(): array();
-    }
 
-    function selectHomeData(){
-        $query = $this->db->select(array('title', 'body', 'image'))->order_by("timestamp", "asc")->get('home', 1);
+    function selectOverallData(){
+        $query = $this->db->select(array('id', 'home_id', 'contact_id'))->order_by("timestamp", "asc")->get('overall');
+        return($query->num_rows() > 0) ? $query->result_array(): array();
+    }
+    function selectHomeData($id){
+        $query = $this->db->select(array('title', 'body', 'image'))->where('id', $id)->get('home');
         return $query->first_row();
         // return($query->num_rows() > 0) ? $query->result_array(): array();
     }
 
-    function selectContactData(){
-        $query = $this->db->select(array('telephone', 'mobile', 'email', 'address'))->order_by("timestamp", "asc")->get('contact', 1);
+    function selectContactData($id){
+        $query = $this->db->select(array('telephone', 'mobile', 'email', 'address'))->where('id', $id)->get('contact');
         return $query->first_row();
         // return($query->num_rows() > 0) ? $query->result_array(): array();
     }
     
+    function selectContentData($id){
+        $query = $this->db->select(array('id', 'overall_id', 'title', 'link'))->where('overall_id', $id)->get('content');
+        return($query->num_rows() > 0) ? $query->result_array(): array();
+    }
+
+    function selectProductData($overallID, $contentID){
+        $query = $this->db->select(array('id', 'name', 'description', 'image'))
+                ->where('overall_id', $overallID)
+                ->where('content_id', $contentID)
+                ->get('body');
+        return($query->num_rows() > 0) ? $query->result_array(): array();
+    }
+
+    function selectVisionData($overallID, $contentID){
+        $query = $this->db->select(array('id', 'name', 'description', 'image'))
+                ->where('overall_id', $overallID)
+                ->where('content_id', $contentID)
+                ->get('body');
+        return($query->num_rows() > 0) ? $query->result_array(): array();
+    }
+
+    function selectProcessData($overallID, $contentID){
+        // $query = $this->db->select(array('step.id', 'step.name', 'step.description', 'step.image', 'sub.id', 'sub.detail'))
+        //                 ->join('sub', 'step.id = sub.step_id')
+        //                 ->get('step');
+        $query = $this->db->select(array('id', 'overall_id', 'content_id', 'name', 'description', 'image'))
+                 ->where('overall_id', $overallID)
+                 ->where('content_id', $contentID)
+                 ->get('body');
+        return($query->num_rows() > 0) ? $query->result_array(): array();
+    }
+
+    function selectProcessDetail($overallID, $contentID, $bodyID){
+        // $query = $this->db->select(array('step.id', 'step.name', 'step.description', 'step.image', 'sub.id', 'sub.detail'))
+        //                 ->join('sub', 'step.id = sub.step_id')
+        //                 ->get('step');
+        $query = $this->db->select(array('id', 'overall_id', 'content_id', 'body_id', 'name', 'description', 'image', 'sub'))
+                 ->where('overall_id', $overallID)
+                 ->where('content_id', $contentID)
+                 ->get('step');
+        return($query->num_rows() > 0) ? $query->result_array(): array();
+    }
 }
 ?>
